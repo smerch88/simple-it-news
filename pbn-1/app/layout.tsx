1f4b2c9eb4f902ed6a49763a9e69b27e930fc8e7
Arsenii <34419542+smerch88@users.noreply.github.com>
2023-10-17 22:54:13 +0300

    feat: add favicon

--- pbn-1/app/layout.tsx
+++ pbn-1/app/layout.tsx
@@ -10,6 +10,25 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Головна',
   description: 'Стрічка IT новин',
+  icons: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      url: '/favicon/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      url: '/favicon/favicon-16x16.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      url: '/favicon/apple-touch-icon.png',
+    },
+  ],
 };
 
 export default function RootLayout({
