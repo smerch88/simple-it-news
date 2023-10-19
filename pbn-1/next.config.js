81665cfc14164eed2d407bcf91663db93f4d965c
Arsenii <34419542+smerch88@users.noreply.github.com>
2023-10-19 23:21:53 +0300

    feat: add security headers

--- pbn-1/next.config.js
+++ pbn-1/next.config.js
@@ -8,6 +8,37 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value:
+              "default-src 'self' 'https://blog.logrocket.com'; image-src 'https://unsplash.com'; script-src 'self' https://www.google-analytics.com; font-src 'self' 'https://fonts.googleapis.com'",
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Permissions-Policy',
+            value:
+              "camera=(); battery=(self); geolocation=(); microphone=('https://a-domain.com')",
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
