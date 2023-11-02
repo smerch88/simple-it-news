902cf296b2d47f7ac6f17720f47775f5d7662da4
smerch88 <maxarmyk@gmail.com>
2023-11-02 22:27:46 +0200

    fix

--- pbn-1/app/robots.ts
+++ pbn-1/app/robots.ts
@@ -4,6 +4,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
+      allow: '/',
     },
     host: 'https://www.simpleitnews.tech',
     sitemap: 'https://www.simpleitnews.tech/sitemap.xml',
