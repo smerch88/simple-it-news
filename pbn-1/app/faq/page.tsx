959e5e54b38cb18774e5d3a986d46cd4ca03f05e
Mykokola <119622278+Mykokola@users.noreply.github.com>
2023-12-04 18:51:36 -0600

    add json

--- pbn-1/app/faq/page.tsx
+++ pbn-1/app/faq/page.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import AskIcon from '@/public/faq/questionIcon.svg';
 import UpIcon from '@/public/faq/state=chevron-up-big.svg';
 import DownIcon from '@/public/faq/state=chevron-down-big.svg';
+import data from '@/data/faq.json';
 export default function Page() {
   return (
     <div className="container">
@@ -36,98 +37,30 @@ export default function Page() {
           </Link>
         </p>
       </div>
-      <ul className=" mt-8">
-        <li className=" cursor-pointer rounded-tl-8  rounded-tr-8 flex gap-2   mb-2  last:mb-0 ">
-          <details className=' w-full relative'>
-            <summary className='  border-b border-dark list-none  text-dark  flex pr-5 pb-2 mb-2  smOnly:text-menuTitleMob   text-t24'>
-              <AskIcon className=" mt-1 h-5 w-5 mr-2 " />
-              Can I use FlowBite in open-source projects?
-              <DownIcon className="absolute  smOnly:top-1 top-2 right-0 bg-white smOnly:h-5  smOnly:w-5  h-5 w-5" />
-            </summary>
-            <p  className='text-grey  smOnly:text-t14  smOnly:max-w-[262px]   mdOnly:text-menuItemsMob max-w-[653px]  text-menuTitleMob'>
-              Generally, it is accepted to use FlowBite in open-source projects,
-              as long as it is not a UI library, a theme, a template, a
-              page-builder that would be considered as an alternative to
-              FlowBite itself.<br />
-              With that being said, feel free to use this design kit for your open-source projects.<br/>
-              Find out more information by <Link className=' text-blue' href='/'> reading the license.</Link>
-            </p>
-            <UpIcon className="absolute  smOnly:top-1 top-2 right-0 bg-white smOnly:h-5 smOnly:w-5  h-5 w-5" />
-          </details>
-        </li>
-        <li className=" cursor-pointer rounded-tl-8  rounded-tr-8 flex gap-2   mb-2  last:mb-0 ">
-          <details className=' w-full relative'>
-            <summary className='  border-b border-dark list-none  text-dark  flex pr-5 pb-2 mb-2  smOnly:text-menuTitleMob   text-t24'>
-              <AskIcon className=" mt-1 h-5 w-5 mr-2 " />
-              Can I use FlowBite in open-source projects?
-              <DownIcon className="absolute  smOnly:top-1 top-2 right-0 bg-white smOnly:h-5  smOnly:w-5  h-5 w-5" />
-            </summary>
-            <p  className='text-grey  smOnly:text-t14  smOnly:max-w-[262px]   mdOnly:text-menuItemsMob max-w-[653px]  text-menuTitleMob'>
-              Generally, it is accepted to use FlowBite in open-source projects,
-              as long as it is not a UI library, a theme, a template, a
-              page-builder that would be considered as an alternative to
-              FlowBite itself.<br />
-              With that being said, feel free to use this design kit for your open-source projects.<br/>
-              Find out more information by <Link className=' text-blue' href='/'> reading the license.</Link>
-            </p>
-            <UpIcon className="absolute  smOnly:top-1 top-2 right-0 bg-white smOnly:h-5 smOnly:w-5  h-5 w-5" />
-          </details>
-        </li>
-        <li className=" cursor-pointer rounded-tl-8  rounded-tr-8 flex gap-2   mb-2  last:mb-0 ">
-          <details className=' w-full relative'>
-            <summary className='  border-b border-dark list-none  text-dark  flex pr-5 pb-2 mb-2  smOnly:text-menuTitleMob   text-t24'>
-              <AskIcon className=" mt-1 h-5 w-5 mr-2 " />
-              Can I use FlowBite in open-source projects?
-              <DownIcon className="absolute  smOnly:top-1 top-2 right-0 bg-white smOnly:h-5  smOnly:w-5  h-5 w-5" />
-            </summary>
-            <p  className='text-grey  smOnly:text-t14  smOnly:max-w-[262px]   mdOnly:text-menuItemsMob max-w-[653px]  text-menuTitleMob'>
-              Generally, it is accepted to use FlowBite in open-source projects,
-              as long as it is not a UI library, a theme, a template, a
-              page-builder that would be considered as an alternative to
-              FlowBite itself.<br />
-              With that being said, feel free to use this design kit for your open-source projects.<br/>
-              Find out more information by <Link className=' text-blue' href='/'> reading the license.</Link>
-            </p>
-            <UpIcon className="absolute  smOnly:top-1 top-2 right-0 bg-white smOnly:h-5 smOnly:w-5  h-5 w-5" />
-          </details>
-        </li>
-        <li className=" cursor-pointer rounded-tl-8  rounded-tr-8 flex gap-2   mb-2  last:mb-0 ">
-          <details className=' w-full relative'>
-            <summary className='  border-b border-dark list-none  text-dark  flex pr-5 pb-2 mb-2  smOnly:text-menuTitleMob   text-t24'>
-              <AskIcon className=" mt-1 h-5 w-5 mr-2 " />
-              Can I use FlowBite in open-source projects?
-              <DownIcon className="absolute  smOnly:top-1 top-2 right-0 bg-white smOnly:h-5  smOnly:w-5  h-5 w-5" />
-            </summary>
-            <p  className='text-grey  smOnly:text-t14  smOnly:max-w-[262px]   mdOnly:text-menuItemsMob max-w-[653px]  text-menuTitleMob'>
-              Generally, it is accepted to use FlowBite in open-source projects,
-              as long as it is not a UI library, a theme, a template, a
-              page-builder that would be considered as an alternative to
-              FlowBite itself.<br />
-              With that being said, feel free to use this design kit for your open-source projects.<br/>
-              Find out more information by <Link className=' text-blue' href='/'> reading the license.</Link>
-            </p>
-            <UpIcon className="absolute  smOnly:top-1 top-2 right-0 bg-white smOnly:h-5 smOnly:w-5  h-5 w-5" />
-          </details>
-        </li>
-        <li className=" cursor-pointer rounded-tl-8  rounded-tr-8 flex gap-2   mb-2  last:mb-0 ">
-          <details className=' w-full relative'>
-            <summary className='  border-b border-dark list-none  text-dark  flex pr-5 pb-2 mb-2  smOnly:text-menuTitleMob   text-t24'>
-              <AskIcon className=" mt-1 h-5 w-5 mr-2 " />
-              Can I use FlowBite in open-source projects?
-              <DownIcon className="absolute  smOnly:top-1 top-2 right-0 bg-white smOnly:h-5  smOnly:w-5  h-5 w-5" />
-            </summary>
-            <p  className='text-grey  smOnly:text-t14  smOnly:max-w-[262px]   mdOnly:text-menuItemsMob max-w-[653px]  text-menuTitleMob'>
-              Generally, it is accepted to use FlowBite in open-source projects,
-              as long as it is not a UI library, a theme, a template, a
-              page-builder that would be considered as an alternative to
-              FlowBite itself.<br />
-              With that being said, feel free to use this design kit for your open-source projects.<br/>
-              Find out more information by <Link className=' text-blue' href='/'> reading the license.</Link>
-            </p>
-            <UpIcon className="absolute  smOnly:top-1 top-2 right-0 bg-white smOnly:h-5 smOnly:w-5  h-5 w-5" />
-          </details>
-        </li>
-            </ul>
+      <ul className="mt-8">
+        {data?.map(({ title, text, link, id }) => (
+          <li
+            key={id}
+            className=" no-select rounded-tl-8 rounded-tr-8  mb-2 flex cursor-pointer   gap-2  last:mb-0 "
+          >
+            <details className=" relative w-full">
+              <summary className="  text-t24 mb-2 flex  list-none  border-b border-dark pb-2 pr-5  text-dark   smOnly:text-menuTitleMob">
+                <AskIcon className=" mr-2 mt-1 h-5 w-5 " />
+                {title}
+                <DownIcon className="absolute  right-0 top-2 h-5 w-5 bg-white  smOnly:top-1  smOnly:h-5 smOnly:w-5" />
+              </summary>
+              <p className="smOnly:text-t14  max-w-[653px]  text-menuTitleMob   text-grey smOnly:max-w-[262px]  mdOnly:text-menuItemsMob">
+                {text}{' '}
+                <Link className=" text-blue" href="/">
+                  {' '}
+                  {link}
+                </Link>
+              </p>
+              <UpIcon className="absolute  right-0 top-2 h-5 w-5 bg-white smOnly:top-1  smOnly:h-5 smOnly:w-5" />
+            </details>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
