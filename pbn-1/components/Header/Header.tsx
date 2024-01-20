01ac908e988aa83b2b1cbb147e46fae9b4ddb18b
smerch88 <maxarmyk@gmail.com>
2024-01-20 16:54:04 +0200

    fix: header position

--- pbn-1/components/Header/Header.tsx
+++ pbn-1/components/Header/Header.tsx
@@ -64,7 +64,7 @@ export const Header = async () => {
                     Контакти
                   </Link>
                 </li>
-                <li className="ml-20">
+                <li className="ml-[300px]">
                   {session?.user?.name ? (
                     <GoogleLogOutButton name={session.user.name} />
                   ) : (
